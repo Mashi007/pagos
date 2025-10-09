a56b20911c65c987538057c0dcd3838dca48c25e
Daniel Casañas <125516119+Mashi007@users.noreply.github.com>
2025-10-09 17:03:53 -0500

    Update __init__.py

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,6 @@
 # app/models/__init__.py
 from app.models.cliente import Cliente
+from app.models.prestamo import Prestamo, EstadoPrestamo, ModalidadPago
 from app.models.pago import Pago
 
-__all__ = ["Cliente", "Pago"]
+__all__ = ["Cliente", "Prestamo", "Pago", "EstadoPrestamo", "ModalidadPago"]
